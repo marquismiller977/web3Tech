ad902820e310f0ffdb85abb3336fd91a86f33d17
marquismiller977 <marquismiller977@gmail.com>
2024-01-23 19:46:58 +1000

    chore: change linke name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and web3
+          Learn React and web3 initial
         </a>
       </header>
     </div>
